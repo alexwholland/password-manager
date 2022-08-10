efcf7711aa47fd4c8e39107d468a86fa2f22262d
Alex Holland <alexanderwholland@uvic.ca>
2022-08-10 14:13:30 -0700

    :sparkles: created simple password GUI

--- manager.py
+++ manager.py
@@ -1,1 +1,22 @@
-print("hello world!")
+import sqlite3, hashlib
+from tkinter import *
+
+window = Tk()
+
+window.title("Password Manager")
+
+def loginScreen():
+    window.geometry("250x100")
+
+    lbl = Label(window, text="Enter Master Password")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+    txt = Entry(window, width=20)
+    txt.pack()
+
+    btn = Button(window, text="Submit")
+    btn.pack(pady=10)
+
+loginScreen()
+window.mainloop()
