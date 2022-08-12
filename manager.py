2a4d789438cdf7df0de8bc33733c8f54abd84619
alexwholland <alexwholland@gmail.com>
2022-08-11 20:03:32 -0700

    :sparkles: Connected password screen to manager screen

--- manager.py
+++ manager.py
@@ -5,18 +5,42 @@ window = Tk()
 
 window.title("Password Manager")
 
+
 def loginScreen():
-    window.geometry("250x100")
+    window.geometry("250x120")
 
     lbl = Label(window, text="Enter Master Password")
     lbl.config(anchor=CENTER)
     lbl.pack()
 
+    # Should be txt = Entry(window, width=20, show="*") in final product
     txt = Entry(window, width=20)
     txt.pack()
+    txt.focus()
 
-    btn = Button(window, text="Submit")
+    lbl1 = Label(window)
+    lbl1.pack()
+
+    def checkPassword():
+        password = "test"
+        if password == txt.get():
+            passwordVault()
+        else:
+            lbl1.config(text="Wrong Password")
+
+    btn = Button(window, text="Submit", command=checkPassword)
     btn.pack(pady=10)
+
+
+def passwordVault():
+    for widget in window.winfo_children():
+        widget.destroy()
+    window.geometry("700x350")
+
+    lbl = Label(window, text="Password Manager")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
 
 loginScreen()
 window.mainloop()
