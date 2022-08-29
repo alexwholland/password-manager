6a2577e5602aa98d3975127831bba59b6a311831
alexwholland <alexwholland@gmail.com>
2022-08-29 09:08:03 -0700

    :sparkles: Added Copy to Clipboard functionality

--- passgen.py
+++ passgen.py
@@ -3,11 +3,19 @@ from random import randint
 
 global_label = None
 
+
 def passGenerator():
     # Password Generator window.
     window = Tk()
 
     window.title("Password Generator")
+
+    def copyToClipboard():
+        """
+        Copy the generated password to the users clipboard
+        """
+        window.clipboard_clear()
+        window.clipboard_append(pwEntry.get())
 
     def createRandomPass():
         """
@@ -26,8 +34,8 @@ def passGenerator():
                 global_label.pack(pady=20)
             return
 
-        #Ideally the global label should be removed after a valid password number is entered
-        #global_label.after(0, global_label.destroy)
+        # Ideally the global label should be removed after a valid password number is entered
+        # global_label.after(0, global_label.destroy)
         for x in range(maxCount):
             password += chr(randint(33, 126))
 
@@ -52,7 +60,7 @@ def passGenerator():
     myButton = Button(myFrame, text="Generate Password", command=createRandomPass)
     myButton.grid(row=0, column=0, padx=10)
 
-    clipBtn = Button(myFrame, text="Copy to Clipboard")
+    clipBtn = Button(myFrame, text="Copy to Clipboard", command=copyToClipboard)
     clipBtn.grid(row=0, column=1, padx=10)
 
     window.mainloop()
